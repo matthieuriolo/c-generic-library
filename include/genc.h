0fc33c3cab6a81077ea2e09002515c322e2bfd54
matthieuriolo <matthieuriolo@388d14b7-9719-0410-a6de-c51569a4ac82>
2011-07-24 20:12:55 +0000

    a header that includes all other headers - an easy way to go - is C++ compatible

---
+++ include/genc.h
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2006, All Rights Reserved, Micah Villmow
+ * @file genc.h
+ * @brief Includes the whole libgenc
+ *
+ */
+
+#ifndef GENC_H_
+#define GENC_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+ 
+#include "gen/access_macros.h"
+#include "gen/control_arr_macros.h"
+#include "gen/control_macros.h"
+#include "gen/data_types.h"
+#include "gen/error_macros.h"
+#include "gen/function_macros.h"
+#include "gen/function_signatures.h"
+#include "gen/gen_macros.h"
+#include "gen/stdin.h"
+
+#include "binary_tree.h"
+#include "comparison.h"
+#include "deque.h"
+#include "hashtable.h"
+#include "heap.h"
+#include "list.h"
+#include "node.h"
+#include "priority_queue.h"
+#include "queue.h"
+#include "stack.h"
+#include "vector.h"
+
+
+#ifdef __cplusplus
+}
+#endif //__cplusplus
+
+#endif //GENC_H_
