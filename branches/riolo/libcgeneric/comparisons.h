9b73dab16a738d9f8617e2f4edf189ce0e426343
matthieuriolo <matthieuriolo@388d14b7-9719-0410-a6de-c51569a4ac82>
2011-09-26 20:44:19 +0000

    fixed issue with the comparisons functions - the problem was that the function statement was inside the headerfile. This made the compiler fail because it retrieves the symbollink twice or even more

--- branches/riolo/libcgeneric/comparisons.h
+++ branches/riolo/libcgeneric/comparisons.h
@@ -9,7 +9,14 @@
  * @brief macro to expand to the comparison function
  * prototype
  */
-#define CMPFUNC_PROTO(TYPE) int TYPE##cmp(void *, void *, size_t)
+ 
+#ifndef COMPARISONS_H_
+#define COMPARISONS_H_
+
+#include "stdint.h"
+#include <stdlib.h>
+
+#define CMPFUNC_PROTO(TYPE) int TYPE##cmp(void * first, void * second, size_t size)
 #define CMPFUNC_IMPL(TYPE) \
 	int TYPE##cmp(void *first, void *second, size_t size) {\
 		return (*(const TYPE *)first) - (*(const TYPE *)second);\
@@ -28,15 +35,5 @@ CMPFUNC_PROTO(uint16_t);
 CMPFUNC_PROTO(uint32_t);
 CMPFUNC_PROTO(uint64_t);
 
-CMPFUNC_IMPL(char)
-CMPFUNC_IMPL(float)
-CMPFUNC_IMPL(double)
-CMPFUNC_IMPL(int8_t)
-CMPFUNC_IMPL(int16_t)
-CMPFUNC_IMPL(int32_t)
-CMPFUNC_IMPL(int64_t)
-CMPFUNC_IMPL(uint8_t)
-CMPFUNC_IMPL(uint16_t)
-CMPFUNC_IMPL(uint32_t)
-CMPFUNC_IMPL(uint64_t)
+#endif
 
---
+++ branches/riolo/src/comparisons.c
@@ -0,0 +1,20 @@
+/**
+ * Copyright 2006, All Rights Reserved, Micah Villmow
+ * @file comparisons.h
+ * @brief function macros to compare the systems datatype
+ */
+
+
+#include "comparisons.h"
+
+CMPFUNC_IMPL(char)
+CMPFUNC_IMPL(float)
+CMPFUNC_IMPL(double)
+CMPFUNC_IMPL(int8_t)
+CMPFUNC_IMPL(int16_t)
+CMPFUNC_IMPL(int32_t)
+CMPFUNC_IMPL(int64_t)
+CMPFUNC_IMPL(uint8_t)
+CMPFUNC_IMPL(uint16_t)
+CMPFUNC_IMPL(uint32_t)
+CMPFUNC_IMPL(uint64_t)
