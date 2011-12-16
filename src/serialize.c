ffbf0c6d717025ba44ec9a263faf7194e9358bd5
matthieuriolo <matthieuriolo@388d14b7-9719-0410-a6de-c51569a4ac82>
2011-12-16 16:22:53 +0000

    encoding support for vector

--- src/serialize.c
+++ src/serialize.c
@@ -4,9 +4,7 @@
 
 TODO:
 
-- think about the point to insert the coder into the API and and remove the CODER parameter in the encoding functions
-- proper FUNC, PROTO ec. declarations
-- change the name formatter to coder
+- think about the point to insert the coder into the API and remove the CODER parameter in the encoding functions
 - make the coder simpler
 	coder {
 		push_begin(obj, size(obj), &length)// obj is passed for the case that you want to insert more information
@@ -18,55 +16,138 @@ TODO:
 
 
 - create the decode AKA read function
+- Create a makro for the standard-coder creators (signatur + prototype)
+
+-rewrite the coder to this structures (rethink about that...):
+
+typedef Coder_t struct {
+	writeContainerStart
+	writeContainerEnd
+	writeContainerValue
+	
+	Coder_t* subcoder;
+} Coder;
+
+
+this structure is more sophisticated because the programmer
+can simpler declare the container typ and the value encoding
+
+- remove the predefined formatter and store them in the files serialize_coder.c / .h
+- rewrite default coder in following matter:
+	Coder* coder = createCoderFormatLineSeparated();//subcoder points to NULL
+	initializeCoderForBase64(coder);//initialize the subcoder
+	//those functions creates a coder with a subcoder.
+	//the maincoder contains as BEGIN and END function the line separating functions.
+	//the subcoder contains an empty BEGIN and END function. The
+	
+- add format Line Separated, XML and JSON
+- add value coding Base64, Base85, HEX, ASCII, Point-Line-Coding (if Bit(x)=0 then print "."; if Bit(x)=1 then print "-"; I guess that this is useful if you're using this library with microcontrollers) 
 */
-
-
-
-
+	
+	
 //predefined formatter
 Coder* createBase64Coder() {
 	Coder* coder = (Coder*)malloc(sizeof(Coder));
-	/*
-	coder->spush=push_base64_start;
-	coder->epush=push_base64_end;
 	
-	coder->sread=read_base64_start;
-	coder->eread=read_base64_end;
-	*/
+	coder->writeContainerBegin = base64_WriteContainerBegin;
+	coder->writeContainerEnd = base64_WriteContainerEnd;
+	coder->writeContainerElement = base64_WriteContainerElement;
+	
 	return coder;
 }
 
 
-
-
-/*
-int serializeArrayToFile(_array_struct* obj, Coder* form, char* path) {
-	FILE* f = fopen(path, "w+");
-	void* data;
-	size_t length;
+int base64_WriteContainerBegin(FILE* file, const void*  obj, size_t size, size_t obj_size) {
+	char* tmp = base64_encode(&size, sizeof(size));
+	size_t ret = fwrite(tmp, strlen(tmp), 1, file);
+	free(tmp);
 	
-	data = form->spush(obj, size(obj), obj, &length);
-	fwrite(data, 1, length, f);
-	free(data);
+	if(ret < 1)
+		return ECODERWRITE;
 	
-	print_func func = API(obj)->print;
-	API(obj)->print = form->print;
-	print(type, obj);
+	if(fwrite("\n", 1, 1, file) < 1)
+		return ECODERWRITE;
 	
-	data = form->epush(obj, size(obj), &length);
-	fwrite(data, 1, length, f);
-	free(data);
+	tmp = base64_encode(&obj_size, sizeof(obj_size));
+	ret = fwrite(tmp, strlen(tmp), 1, file);
+	free(tmp);
 	
-	API(obj)->print = func;
+	if(ret < 1)
+		return ECODERWRITE;
+	
+	if(fwrite("\n", 1, 1, file) < 1)
+		return ECODERWRITE;
 	
 	return SUCCESS;
 }
 
-int serializeListToFile(_array_struct* obj, SerializeFormatter* form, char* path) {
-	
+
+int base64_WriteContainerEnd(FILE* file, const void* obj, size_t size, size_t obj_size) {
+	return SUCCESS;
 }
 
-int unserializeArrayFromFile(_array_struct* obj, SerializeFormatter* form, char* path) {}
+int base64_WriteContainerElement(FILE* file, const void* elem, size_t size) {
+	char* tmp = base64_encode(elem, size);
+	size_t ret = fwrite(tmp, strlen(tmp), 1, file);
+	free(tmp);
+	
+	if(ret < 1)
+		return ECODERWRITE;
+	
+	if(fwrite("\n", 1, 1, file) < 1)
+		return ECODERWRITE;
+	
+	return SUCCESS;
+}
 
-int serializeListFromFile(_array_struct* obj, SerializeFormatter* form, char* path) {}
+
+
+Coder* createXMLCoder() {
+	Coder* coder = (Coder*)malloc(sizeof(Coder));
+	
+	coder->writeContainerBegin = xml_WriteContainerBegin;
+	coder->writeContainerEnd = xml_WriteContainerEnd;
+	coder->writeContainerElement = xml_WriteContainerElement;
+	
+	return coder;
+}
+
+
+int xml_WriteContainerBegin(FILE* file, const void*  obj, size_t size, size_t obj_size) {
+	if(fprintf(file, "<data count=\"%lu\" size=\"%lu\">\n", size, obj_size)==0)
+		return ECODERWRITE;
+	
+	return SUCCESS;
+}
+
+
+int xml_WriteContainerEnd(FILE* file, const void* obj, size_t size, size_t obj_size) {
+	if(fprintf(file, "</data>\n")==0)
+		return ECODERWRITE;
+	
+	return SUCCESS;
+}
+
+/* You've to overwrite this function for a proper xml representation (with proper is meant that it is readable for human) */
+int xml_WriteContainerElement(FILE* file, const void* elem, size_t size) {
+	char* tmp = base64_encode(elem, size);
+	size_t ret = fprintf(file, "\t<item>%s</item>\n", tmp);
+	free(tmp);
+	
+	if(ret == 0)
+		return ECODERWRITE;
+	
+	return SUCCESS;
+}
+
+
+
+// -- datatypes --
+
+FUNC_ENCODE(Vector)
+
+/*
+FUNC_ENCODE(BinaryTree)
+FUNC_ENCODE(Queue)
+...
 */
